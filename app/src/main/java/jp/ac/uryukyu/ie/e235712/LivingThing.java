e5689b0653c42fb83236098bc132d65d4cdc053b
Maruo Naoki <e235712@ie.u-ryukyu.ac.jp>
2023-11-27 19:22:47 +0900

    report4 step1

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235712/LivingThing.java
@@ -0,0 +1,46 @@
+package jp.ac.uryukyu.ie.e235712;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    public LivingThing(String name, int hitPoint, int attack) {
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.attack = attack;
+        this.dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+    
+    public void attack(LivingThing opponent) {
+        if (!dead) {
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if (hitPoint <= 0) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+}
+    
+
